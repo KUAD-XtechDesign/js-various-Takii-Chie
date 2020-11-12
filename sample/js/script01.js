a6ad4053018697d1edac3b36257515cbf8a1aca0
Takii_Chie <68629404+Takii-Chie@users.noreply.github.com>
2020-11-12 22:25:47 +0900

    あ

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -10,10 +10,10 @@ $(function(){
 
   function contentAnime(){
     setTimeout(backAnime,300);//0.3秒後backAnime実行
-    setTimeout(personAnime,2000);//2秒後personAnime実行
-    setTimeout(woodAnime,2000);//2秒後woodAnime実行
-    setTimeout(carAnime,2000);//2秒後carAnime実行
-    setTimeout(foodAnime,3000);//3秒後foodAnime実行
+    setTimeout(inosisiAnime,400);//2秒後inosisiAnime実行
+    setTimeout(woodAnime,400);//2秒後woodAnime実行
+    setTimeout(uriboAnime,400);//2秒後uriboAnime実行
+    setTimeout(foodAnime,1500);//3秒後foodAnime実行
   }
   
   //以下のアニメはCSSでやってみる
@@ -22,16 +22,16 @@ $(function(){
     $("#back").addClass("anime")
   }
 
-  function personAnime(){
-    $("#person").addClass("anime")
+  function inosisiAnime(){
+    $("#inosisi").addClass("anime")
   }
 
   function woodAnime(){
     $("#wood").addClass("anime")
   }
 
-  function carAnime(){
-    $("#car").addClass("anime")
+  function uriboAnime(){
+    $("#uribo").addClass("anime")
   }
 
   function foodAnime(){
